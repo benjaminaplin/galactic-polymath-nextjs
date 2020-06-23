7bab6c3a9188a452a4f0d1fbd1e646e195b0e8f9
benjaminaplin <benjaminaplin@gmail.com>
2020-06-23 12:32:34 -0500

    basic parallax with responsiveness working

--- pages/landingPage/index.js
+++ pages/landingPage/index.js
@@ -1,4 +1,4 @@
-import React, { Component , useRef} from "react";
+import React, { useEffect, useRef, useState } from "react";
 import MultiLineLessons from "../../components/gpComponents/multiLineLessons";
 import Layout from "../../components/layout/layout";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
@@ -12,11 +12,25 @@ const calc = (o) => `translateY(${o * 0.5}px)`;
 
 const LandingPageBody = () => {
   const ref = useRef()
-  
+    const [viewPortWidth, setViewPortWidth] = useState(320)
+
+  useEffect(() => {
+    const vw = document.documentElement.clientWidth;
+    setViewPortWidth(vw)
+  }, [])
+
+  useEffect(() => {
+      document.getElementsByTagName("body")[0].style.padding = 0;
+    return () => {
+      document.getElementsByTagName("body")[0].style.padding = "2rem 1rem";
+    }
+  }, [])
+
+  console.log('viewPortWidth', viewPortWidth)
   return (
     <>
       <Parallax
-        pages={4}
+        pages={viewPortWidth < 400 ? 6.75 : 4.35}
         ref={(ref) => ref}
         style={{ backgroundColor: colors.gPSparkleWhite }}
       >
@@ -26,31 +40,16 @@ const LandingPageBody = () => {
             <img className="header-logo" src="https://gdurl.com/4KdN" />
           </div>
         </ParallaxLayer>
-        <ParallaxLayer offset={.05} speed={1}>
-          <div className="diagonal-box">offset .05 layer</div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={1.5} speed={.1}>
-          <div className="diagonal-box-2">offset 1.5 layer</div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={1.75} speed={2}>
-          <div className="diagonal-box">offset 1.75 layer</div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={2} speed={1}>
-          <div className="diagonal-box-2">offset 2 layer</div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={3} speed={2}>
-          <div className="diagonal-box">offset 3 layer</div>
-        </ParallaxLayer>
         <div className="landing-page-body-grid">
-            <img className="classroom-image" src="https://gdurl.com/9GiJ" />
-            <div className="mission-statement-text">
-              <p>
-                <strong>We are a startup education studio. </strong>
-                We think learning should be fun. When it&rsquo;s not fun, it
-                should be interesting. And to be interesting, it needs to connect
-                to the real world.
-              </p>
-            </div>
+          <img className="classroom-image" src="https://gdurl.com/9GiJ" />
+          <div className="mission-statement-text">
+            <p>
+              <strong>We are a startup education studio. </strong>
+              We think learning should be fun. When it&rsquo;s not fun, it
+              should be interesting. And to be interesting, it needs to connect
+              to the real world.
+            </p>
+          </div>
           <div className="stem-image-row">
             <img className="stem-image" src="https://gdurl.com/UWKg" />
           </div>
