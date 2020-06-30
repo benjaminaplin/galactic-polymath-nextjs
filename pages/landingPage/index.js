c22e227029e550a86052695f387a156d92e22a14
Ben Aplin <benjaminaplin@gmail.com>
2020-06-30 16:50:38 -0500

    rm parallax (#20)

--- pages/landingPage/index.js
+++ pages/landingPage/index.js
@@ -6,79 +6,45 @@ import { useSpring, animated } from "react-spring";
 import NavBar from '../../components/layout/navBar'
 import Footer from '../../components/layout/footer'
 import colors from '../../styles/colors'
-// You can use this `calc` method to increase the impact
-// of the effect by playing around with the values and units.
-const calc = (o) => `translateY(${o * 0.5}px)`;
 
-// height of container as rendered 3752 - height of container as it should be 3282
-//470
-// 3282
 const LandingPageBody = () => {
-  const ref = useRef()
-    const [viewPortWidth, setViewPortWidth] = useState(320)
-    const [numPages, setNumPages] = useState(320)
-
-  useEffect(() => {
-    const vw = document.documentElement.clientWidth;
-    setViewPortWidth(vw)
-  }, [])
-
   return (
-    <div>
-      <Parallax
-        // pages={viewPortWidth < 350 ? 5.5 : 4}
-        pages={3.5}
-        ref={(ref) => ref}
-        style={{
-          backgroundColor: colors.gPSparkleWhite,
-          width: "100%",
-          // height: "3282px",
-        }}
-      >
-        <NavBar />
-        <div style={{ width: viewPortWidth }}>
-          <ParallaxLayer style={{ width: "100%" }} offset={0} speed={-0.5}>
-            <div className="header-logo-container">
-              <img className="header-logo" src="https://gdurl.com/4KdN" />
-            </div>
-          </ParallaxLayer>
+    <div>   
+      <NavBar />
+      <div className="header-logo-container">
+        <img className="header-logo" src="https://gdurl.com/4KdN" />
+      </div>
+      <div className="landing-page-body-grid">
+        <img className="classroom-image" src="https://gdurl.com/9GiJ" />
+        <div className="mission-statement-text">
+          <p>
+            <strong>We are a startup education studio. </strong>
+            We make lessons that are{" "}
+            <i>creative, authentic, and interdisciplinary</i>. We
+            translate knowledge that is not generally available to the
+            public by working directly with experts.
+          </p>
         </div>
-        <ParallaxLayer factor={3}>
-          <div>
-            <div className="landing-page-body-grid">
-              <img className="classroom-image" src="https://gdurl.com/9GiJ" />
-              <div className="mission-statement-text">
-                <p>
-                  <strong>We are a startup education studio. </strong>
-                  We make lessons that are{" "}
-                  <i>creative, authentic, and interdisciplinary</i>. We
-                  translate knowledge that is not generally available to the
-                  public by working directly with experts.
-                </p>
-              </div>
-              <div className="stem-image-row">
-                <img className="stem-image" src="/stem.png" />
-              </div>
-              <div className="summary-statement-row">
-                <p>
-                  We work directly with experts to translate complex knowledge
-                  into rigorous, but fun,{" "}
-                  <strong>standards-aligned learning experiences</strong> for
-                  students.
-                </p>
-                <p>
-                  Here&rsquo;s our first unit-made up of 4 interdisciplinary
-                  lessons (a math, ELA, science, and social studies lesson)
-                  built around a brand new study on birdsong, and centering on
-                  the theme of how our assumptions affect our conclusions.
-                </p>
-              </div>
-              <MultiLineLessons />
-              <Footer />
-            </div>
-          </div>
-        </ParallaxLayer>
-      </Parallax>
+        <div className="stem-image-row">
+          <img className="stem-image" src="/stem.png" />
+        </div>
+        <div className="summary-statement-row">
+          <p>
+            We work directly with experts to translate complex knowledge
+            into rigorous, but fun,{" "}
+            <strong>standards-aligned learning experiences</strong> for
+            students.
+          </p>
+          <p>
+            Here&rsquo;s our first unit-made up of 4 interdisciplinary
+            lessons (a math, ELA, science, and social studies lesson)
+            built around a brand new study on birdsong, and centering on
+            the theme of how our assumptions affect our conclusions.
+          </p>
+        </div>
+        <MultiLineLessons />
+        <Footer />
+      </div>
     </div>
   );
 }
