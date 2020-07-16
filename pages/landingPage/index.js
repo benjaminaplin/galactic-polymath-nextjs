ea24a38556431fbb9a20c311d5882f3b5c178302
Ben Aplin <benjaminaplin@gmail.com>
2020-07-16 16:42:54 -0500

    Update styles (#21)
    * add parallax
    
    * update styles
    
    * add cloudinary urls

--- pages/landingPage/index.js
+++ pages/landingPage/index.js
@@ -1,21 +1,59 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect } from "react";
 import MultiLineLessons from "../../components/gpComponents/multiLineLessons";
 import Layout from "../../components/layout/layout";
-import { Parallax, ParallaxLayer } from "@react-spring/parallax";
-import { useSpring, animated } from "react-spring";
-import NavBar from '../../components/layout/navBar'
-import Footer from '../../components/layout/footer'
-import colors from '../../styles/colors'
+import { makeCloudinaryUrl } from "../../shared/constants";
 
 const LandingPageBody = () => {
+  const inViewPort = (el) => {
+    let rect = el.getBoundingClientRect();
+    return (
+      (rect.top <= 0 && rect.bottom >= 0) ||
+      (rect.bottom >= window.innerHeight && rect.top <= window.innerHeight) ||
+      (rect.top >= 0 && rect.bottom <= window.innerHeight)
+    );
+  };
+
+  const handleScroll = () => {
+    const top = window.pageYOffset;
+    const headerLogoContainer = document.querySelector(
+      ".header-logo-container"
+    );
+    headerLogoContainer.style.transform = `translateY(${top / 1.5}px)`;
+    headerLogoContainer.style.opacity =
+      1 - Math.max(top / (window.innerHeight * 0.3), 0);
+    const mathLessonImage = document.querySelector(".math-lesson-image");
+    // console.log("isInView?", inViewPort(mathLessonImage));
+  };
+
+  useEffect(() => {
+    const watchScroll = () => {
+      window.addEventListener("scroll", handleScroll);
+    };
+    watchScroll();
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
   return (
     <Layout>
       <>
         <div className="header-logo-container">
-          <img className="header-logo" src="https://gdurl.com/4KdN" />
+          {/* <img className="header-logo" src="https://gdurl.com/4KdN" /> */}
+          <img
+            className="header-logo"
+            src={makeCloudinaryUrl(
+              "v1594935064/logos/GP-bl_text_rainbow_icon_alpha_bg-01_kfbga4.png"
+            )}
+          />
         </div>
         <div className="landing-page-body-grid">
-          <img className="classroom-image" src="https://gdurl.com/9GiJ" />
+          <img
+            className="classroom-image"
+            src={makeCloudinaryUrl(
+              "v1592966103/Flowers_NIT_classroom_300ppi_ex64rj.png"
+            )}
+          />
           <div className="mission-statement-text">
             <p>
               <strong>We are a startup education studio. </strong>
@@ -26,7 +64,12 @@ const LandingPageBody = () => {
             </p>
           </div>
           <div className="stem-image-row">
-            <img className="stem-image" src="/stem.png" />
+            <img
+              className="stem-image"
+              src={makeCloudinaryUrl(
+                "v1592966102/GP_Visual_Argument_sw6hv1.png"
+              )}
+            />
           </div>
           <div className="summary-statement-row">
             <p>
@@ -47,7 +90,6 @@ const LandingPageBody = () => {
       </>
     </Layout>
   );
-}
-
+};
 
 export default LandingPageBody;
